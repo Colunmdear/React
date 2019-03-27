1e3e0a4ff81076cc668cd4521be952577ba75339
liugen <lf853970398@gmail.com>
2019-03-27 09:39:36 +0800

    初始化

---
+++ src/components/new.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react'
+
+import PropTypes from 'prop-types'
+
+import {styled} from 'styled-components'
+
+class Content extends Component{
+  render () {
+    return (
+     <div>
+  
+     </div>
+   ) 
+ } 
+} 
+Content.propTypes={
+  title:PropTypes.string
+}
+export default Content
